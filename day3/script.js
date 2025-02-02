ff313db8e07f4eb1499e37110e5425c6c4a30566
unknown <manishaaloori@gmail.com>
2025-02-02 16:37:42 +0530

    msg

--- day3/script.js
+++ day3/script.js
@@ -1,15 +1,19 @@
+
+console.log("print even numbers from 1 to 20: ")
 for(i=1;i<=20;i++){
     if(i%2==0){
         console.log(i)
     }
 }
 
+console.log("print multiples of 5 up to 50: ")
 for(i=1;i<=50;i++){
     if(i%5==0){
         console.log(i)
     }
 }
 
+console.log("calculate factorial: ")
 fact=1
 for(i=5;i<=1;i--){
     console.log(i)
@@ -18,12 +22,15 @@ for(i=5;i<=1;i--){
 console.log(fact)
 
 
+console.log("multiplication table(1 to 5): ")
+
 for(i=1;i<=5;i++){
 for(j=1;j<=10;j++){
 console.log(i+"*"+j+"="+i*j);
 }
 }
 
+console.log("print alphabet: ")
 fact=""
 for(i=65;i<=90;i++){
     fact+=String.fromCharCode(i);
--- day5/script.js
+++ day5/script.js
@@ -1,13 +1,15 @@
-console.log("Try programiz.pro");
-prime=30
-count=true;
-for(i=2;i<prime;i++){
-    if(prime%i==0){
-        console.log(i,prime,"not prime")
-        count=false;
-        break
+
+
+console.log("PRIME NUMBER 2 TO 30: ")
+for(num=2;num<=30;num++){
+    count=true;
+    for(i=2;i<num;i++){
+        if(num%i==0){
+            count=false;
+            break
+        }
     }
-}
-if(count){
-    console.log(prime,"is prime number")
+    if(count){
+        console.log(num,"is prime")
+    }
 }
